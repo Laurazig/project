2528b13f739bdb07de8c1955ce0128a4dc2186fa
Laura <lhzig@protonmail.com>
2022-07-02 13:32:41 +0200

    no p

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import mongoose from "mongoose"
 const app = express();
 
 //mongoose.connect("mongodb://localhost:27017/fullstack-project")  
-mongoose.connect("mongodb+srv://n42:Bb!ongo1515@cluster0.r46gvie.mongodb.net/?retryWrites=true&w=majority") 
+mongoose.connect("mongodb+srv://n42:<password>@cluster0.r46gvie.mongodb.net/?retryWrites=true&w=majority") 
 mongoose.connection.on("open", ()=> console.log("Database  connection established"));
 mongoose.connection.on("error",()=> console.error);
 
